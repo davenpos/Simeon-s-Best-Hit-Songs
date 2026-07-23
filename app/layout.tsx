1ead9f6dd8d21725bc42d5c80f83ff324580d3ab
davenpos <simmywim@hotmail.com>
2026-07-23 13:20:34 -0400

    Created public folder and added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/assets/global.css';
+import '@/public/global.css';
 
 export const metadata = {
   title: "Simeon's Best Hit Songs",
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="Norbert.ico" sizes="any" />
+      </head>
       <body className="min-h-screen bg-linear-to-b from-lime-600 via-lime-500 to-lime-700 text-white antialiased">
         <main className="mx-auto max-w-5xl px-4 py-8 sm:px-6 sm:py-10">{children}</main>
       </body>
